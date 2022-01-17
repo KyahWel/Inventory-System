922577dfc773cb2f3fe2dbae17d5b8f1f9a26f6b
William <williamcris18@gmail.com>
2022-01-17 20:45:36 +0800

    Updated schema

--- schema/payroll-system.sql
+++ schema/payroll-system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 17, 2022 at 11:04 AM
+-- Generation Time: Jan 17, 2022 at 12:44 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `payroll-system`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin-accounts`
+--
+
+DROP TABLE IF EXISTS `admin-accounts`;
+CREATE TABLE IF NOT EXISTS `admin-accounts` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `dateAdded` date NOT NULL,
+  `timeAdded` time NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin-accounts`
+--
+
+INSERT INTO `admin-accounts` (`id`, `username`, `password`, `firstname`, `lastname`, `dateAdded`, `timeAdded`) VALUES
+(1, 'admin', 'admin', 'William Cris', 'Hod', '2022-01-15', '14:08:29');
 
 -- --------------------------------------------------------
 
@@ -50,6 +75,55 @@ CREATE TABLE IF NOT EXISTS `employee-accounts` (
 
 INSERT INTO `employee-accounts` (`employeeID`, `employeeNumber`, `firstname`, `lastname`, `age`, `address`, `position`, `sss_number`, `pagibig_number`, `philhealth_number`, `tin_number`, `employmentDate`) VALUES
 (1, 'Employee-8667', 'William Cris', 'Hod', '20', '', 'driver', '132', '123', '123', '123', '2022-01-27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee-payout`
+--
+
+DROP TABLE IF EXISTS `employee-payout`;
+CREATE TABLE IF NOT EXISTS `employee-payout` (
+  `payoutID` int(255) NOT NULL AUTO_INCREMENT,
+  `employeeID` int(255) NOT NULL,
+  `date-received` date NOT NULL,
+  `amount` float NOT NULL,
+  PRIMARY KEY (`payoutID`),
+  KEY `employee-payout` (`employeeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event-log`
+--
+
+DROP TABLE IF EXISTS `event-log`;
+CREATE TABLE IF NOT EXISTS `event-log` (
+  `eventID` int(255) NOT NULL AUTO_INCREMENT,
+  `employeeID` int(255) NOT NULL,
+  `event` varchar(255) NOT NULL,
+  `time-happened` time NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`eventID`),
+  KEY `employee-log` (`employeeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employee-payout`
+--
+ALTER TABLE `employee-payout`
+  ADD CONSTRAINT `employee-payout` FOREIGN KEY (`employeeID`) REFERENCES `employee-accounts` (`employeeID`);
+
+--
+-- Constraints for table `event-log`
+--
+ALTER TABLE `event-log`
+  ADD CONSTRAINT `employee-log` FOREIGN KEY (`employeeID`) REFERENCES `employee-accounts` (`employeeID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
