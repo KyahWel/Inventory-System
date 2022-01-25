93688c9c158a12e3bbacd35103bfa15c0b93d34c
Czarina Pielago <91414723+czareen@users.noreply.github.com>
2022-01-26 01:23:54 +0800

    Modified event log tab

---
+++ assets/js/Eventlog.js
@@ -0,0 +1,10 @@
+function mainEvent() {
+    document.getElementById('mainEvent').style.display = "block";
+    document.getElementById('viewEvent').style.display = "none";
+}
+
+function viewEvent() {
+    document.getElementById('mainEvent').style.display = "none";
+    document.getElementById('viewEvent').style.display = "block";
+}
+
